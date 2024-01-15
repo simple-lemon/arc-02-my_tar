3be87ed31ae333e09a72f0859df8acbc10bd4f30
Nurulloh <140059040+abdurashidovnurulloh@users.noreply.github.com>
2024-01-15 09:03:01 +0500

    Update linked_list.h

--- h/linked_list.h
+++ h/linked_list.h
@@ -1,113 +1,70 @@
-#include "../h/linked_list.h"
-#include "../h/my_string.h"
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 
-void tar_null(posix_header* list) {
-    null(list->name, 100);
-    null(list->mode, 8);
-    null(list->uid, 8);
-    null(list->gid, 8);
-    null(list->size, 12);
-    null(list->mtime, 12);
-    null(list->chksum, 8);
-    null(list->linkname, 100);
-    null(list->magic, 6);
-    null(list->version, 2);
-    null(list->uname, 32);
-    null(list->gname, 32);
-    null(list->devmajor, 8);
-    null(list->devminor, 8);
-    null(list->prefix, 155);
-    null(list->add, 12);
-}
+#include <stdlib.h>
 
-void add_list(lists** main_list, char* name) 
+#ifndef POSIX_HEADER
+#define POSIX_HEADER
+
+typedef struct posix_header {                              
+  char name[100];
+  char mode[8];
+  char uid[8];                  
+  char gid[8];                   
+  char size[12];                
+  char mtime[12];
+  char chksum[8];               
+  char typeflag;                
+  char linkname[100];           
+  char magic[6];                
+  char version[2];              
+  char uname[32];               
+  char gname[32];               
+  char devmajor[8];             
+  char devminor[8];             
+  char prefix[155];             
+  char add[12];          
+} posix_header;
+
+#endif
+
+#ifndef LISTS
+#define LISTS
+
+typedef struct listnode 
 {
-    lists* new = (lists*)malloc(sizeof(lists));
-    new->next = NULL;
-    new->name = name;
-    if(!(*main_list)) 
-    {
-        *main_list = new;
-    }
-    else 
-    {
-        lists* head = *main_list;
-        for( ; head->next; head = head->next) {}
-        head->next = new;
-    }
-}
+    char* name;
+    struct listnode*next;
+} lists;
 
-void clean_list(lists* list) 
-{
-    lists* temp = NULL;
-    while(list) 
-    {
-        temp = list;
-        list = list->next;
-        temp->name = NULL;
-        free(temp);
-    }
-}
+#endif
+#ifndef LIST_TAR
+#define LIST_TAR
 
-void tar_list_add(list_tar** list, posix_header* f_info) 
-{
-    list_tar* new = (list_tar*)malloc(sizeof(list_tar));
-    new->name_tar_list = f_info;
-    new->next = NULL;
-    if(!(*list)) 
-    {
-        *list = new;
-    }
-    else 
-    {
-        list_tar* head = *list;
-        for( ; head->next; head = head->next) {}
-        head->next = new;
-    }
-}
+typedef struct list {
+    struct list* next;
+    posix_header* name_tar_list;
+} list_tar;
 
-void clean_tar_list(list_tar* list) 
-{
-    list_tar* temp;
-    while(list) 
-    {
-        tar_null(list->name_tar_list);
-        free(list->name_tar_list);
-        temp = list;
-        list = list->next;
-        free(temp);
-    }
-}
+#endif
 
-void add_tar_content_list(list_tar_content** list, char* name, char* mtime) 
-{
-    list_tar_content* new = (list_tar_content*)malloc(sizeof(list_tar_content));
-    new->name = (char*)calloc(sizeof(char), (my_strlen(name) + 1));
-    new->name = my_strcpy(new->name, name);
-    new->mtime = (char*)calloc(sizeof(char), (my_strlen(mtime) + 1));
-    new->mtime = my_strcpy(new->mtime, mtime);
-    new->next = NULL;
-    if(*list == NULL) 
-    {
-        *list = new;
-    }
-    else 
-    {
-        list_tar_content* head = *list;
-        for( ; head->next; head = head->next) {}
-        head->next = new;
-    }
-}
+#ifndef LIST_TAR_CONTENT
+#define LIST_TAR_CONTENT
 
-void list_tar_content_clean(list_tar_content* list) 
-{
-    list_tar_content* temp;
-    while(list) 
-    {
-        temp = list;
-        list = list->next;
-        free(temp->name);
-        free(temp->mtime);
-        free(temp);
-    }
-}
+typedef struct list_tar {
+    struct list_tar* next;
+    char* name;
+    char* mtime;
+} list_tar_content;
+
+#endif
+
+void add_list(lists** main_list, char* name);
+void clean_list(lists* list);
+void tar_list_add(list_tar** list, posix_header* f_info);
+void clean_tar_list(list_tar* list);
+void add_tar_content_list(list_tar_content** list, char* name, char* mtime);
+void list_tar_content_clean(list_tar_content* list);
+void tar_null(posix_header* list);
+
+#endif
